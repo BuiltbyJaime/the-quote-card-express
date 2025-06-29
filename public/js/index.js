0f6523f33e8fdf574540ddfe029a69c804f092e4
Jaime <jai01me@me.com>
2025-06-29 00:39:41 -0400

    included changes to hide API key

--- public/js/index.js
+++ public/js/index.js
@@ -5,21 +5,33 @@ const elements = {
     author: document.getElementById("author"),
 };
 
+// async function getRandomImage() {
+//     const client_id = "Fji7geOljbvIblKcDYJogK4B9mM9m9qwNrTX9CINxh0";
+//     const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+//     try {
+//         const response = await fetch(endpoint);
+//         const returnedData = await response.json();
+//         const receivedPhotoUrl = returnedData.urls.regular;
+
+//         const imgDiv = document.querySelector(".background-img");
+//         imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
 async function getRandomImage() {
-    const client_id = "Fji7geOljbvIblKcDYJogK4B9mM9m9qwNrTX9CINxh0";
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    const endpoint = "http://localhost:8080/api/v1/getRandomImage";
     try {
         const response = await fetch(endpoint);
         const returnedData = await response.json();
-        const receivedPhotoUrl = returnedData.urls.regular;
+        const receivedPhotoUrl = returnedData.data;
 
         const imgDiv = document.querySelector(".background-img");
-        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+        imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
     } catch (error) {
         console.error(error);
     }
 }
-
 
 getRandomImage();
 
